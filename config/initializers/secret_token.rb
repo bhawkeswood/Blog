e6ea6d6a460d288553dea70820f7ffbdc8979097
bhawkeswood <bhawkeswood@gmail.com>
2013-09-15 13:14:02 -0400

    Initial commit, but incorrectly setup repository previously

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'de4430fc0d4dcfc6dd139f3fdee68e7210ed824f1e3d525d2bb681dc02ae34b32addfa4d406c6f1b2856932bc0ed23f0714854b57503c1ef3febec5c9a31280c'
