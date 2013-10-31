80cdb2dba371d9ee3b395c7573e3254db2913829
bhawkeswood <bhawkeswood@gmail.com>
2013-10-30 22:39:20 -0400

    Generating the sorcery initializers and user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915154556) do
+ActiveRecord::Schema.define(version: 20131031022805) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username",         null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
