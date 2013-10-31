6041e7fd722f724f6435c69520799d841eb6000f
bhawkeswood <bhawkeswood@gmail.com>
2013-10-30 22:01:49 -0400

    Adding editing and deleting options

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,9 +27,25 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update_attributes post_params
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy 
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :text)
 		end
-
 end
